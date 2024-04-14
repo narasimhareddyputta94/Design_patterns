da33df240fe980172ba7c396b59a29d84f88fd45
narasimha reddy putta <150554709+narasimhareddyputta94@users.noreply.github.com>
2024-04-14 17:01:52 -0500

    implemeted double check Locking

---
+++ src/main/java/Singleton_design_pattern/doubleCheckLocking/DataBaseConnection.java
@@ -0,0 +1,28 @@
+package Singleton_design_pattern.doubleCheckLocking;
+
+public class DataBaseConnection {
+    private static DataBaseConnection instance = null;
+
+    private DataBaseConnection() {
+        // private constructor to prevent instantiation
+    }
+
+    public static DataBaseConnection getInstance() {
+        if (instance == null) {
+            synchronized (DataBaseConnection.class) {
+                if (instance == null) {
+                    instance = new DataBaseConnection();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public void connect() {
+        System.out.println("Connected to database");
+    }
+
+    public void disconnect() {
+        System.out.println("Disconnected from database");
+    }
+}
