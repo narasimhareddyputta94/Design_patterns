56c9ac96e1e23c42b54dff2384c7da2498a9d681
narasimha reddy putta <150554709+narasimhareddyputta94@users.noreply.github.com>
2024-04-14 17:14:16 -0500

    completed singletone pattern

--- src/main/java/Singleton_design_pattern/doubleCheckLocking/Client.java
+++ src/main/java/Singleton_design_pattern/doubleCheckLocking/Client.java
@@ -2,9 +2,28 @@ package Singleton_design_pattern.doubleCheckLocking;
 
 public class Client {
     public static void main(String[] args) {
-        DataBaseConnection connection1 = DataBaseConnection.getInstance();
-        connection1.connect();
-        DataBaseConnection connection2 = DataBaseConnection.getInstance();
-        connection2.disconnect();
+        Thread thread1 = new Thread(() -> {
+            DataBaseConnection connection1 = DataBaseConnection.getInstance();
+            connection1.connect();
+        });
+
+        Thread thread2 = new Thread(() -> {
+            DataBaseConnection connection2 = DataBaseConnection.getInstance();
+            connection2.disconnect();
+        });
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("End of main");
+
+
     }
 }
