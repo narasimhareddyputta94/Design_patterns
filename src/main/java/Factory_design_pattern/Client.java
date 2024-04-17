cf99f0d6a629555507f27b5a51d836c5e04106f4
narasimha reddy putta <150554709+narasimhareddyputta94@users.noreply.github.com>
2024-04-17 14:56:15 -0500

    implemented all creational design patterns

--- src/main/java/Factory_design_pattern/Client.java
+++ src/main/java/Factory_design_pattern/Client.java
@@ -3,10 +3,21 @@ import Factory_design_pattern.components.button.Button;
 
 public class Client {
     public static void main(String[] args) {
-        Flutter flutter = new Flutter(SupportedPlatforms.IOS);
-        UIFactory uiFactory = flutter.createUIFactory();
 
+        Flutter flutter = new Flutter(SupportedPlatforms.ANDROID);
+        UIFactory uiFactory = flutter.createUIFactory();
         Button button = uiFactory.createButton();
+        button.render();
+
+        flutter = new Flutter(SupportedPlatforms.IOS);
+        uiFactory = flutter.createUIFactory();
+        button = uiFactory.createButton();
+        button.render();
+
+        flutter = new Flutter(SupportedPlatforms.WEB);
+        uiFactory = flutter.createUIFactory();
+        button = uiFactory.createButton();
+        button.render();
 
 
 
