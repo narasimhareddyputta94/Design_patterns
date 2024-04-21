95781a75224c366cbb0db5089f1754a65f15e926
narasimha reddy putta <150554709+narasimhareddyputta94@users.noreply.github.com>
2024-04-21 14:23:42 -0500

    done

--- src/main/java/decorator_design_pattern/IceCreamSoftware/Client.java
+++ src/main/java/decorator_design_pattern/IceCreamSoftware/Client.java
@@ -22,7 +22,22 @@ public class Client {
         IceCream iceCream3 = new StrawBerry(new vanillachips(new Choclchips(new AlcholCone())));
         System.out.println(iceCream3.getDiscription() + " Cost: " + iceCream3.getCost());
 
+        IceCream iceCream4 = new StrawBerry(new vanillachips(new Choclchips(new CannabisCone())));
+        System.out.println(iceCream4.getDiscription() + " Cost: " + iceCream4.getCost());
 
+        IceCream iceCream5 = new StrawBerry(new vanillachips(new Choclchips(new VanillaCone())));
+        System.out.println(iceCream5.getDiscription() + " Cost: " + iceCream5.getCost());
 
+        IceCream iceCream6 = new StrawBerry(new vanillachips(new Choclchips(new BlueBeeryCone())));
+        System.out.println(iceCream6.getDiscription() + " Cost: " + iceCream6.getCost());
+
+        IceCream iceCream7 = new StrawBerry(new vanillachips(new Choclchips(new AlcholCone())));
+        System.out.println(iceCream7.getDiscription() + " Cost: " + iceCream7.getCost());
+
+        IceCream iceCream8 = new StrawBerry(new vanillachips(new Choclchips(new CannabisCone())));
+        System.out.println(iceCream8.getDiscription() + " Cost: " + iceCream8.getCost());
+
+        IceCream iceCream9 = new StrawBerry(new vanillachips(new Choclchips(new VanillaCone())));
+        System.out.println(iceCream9.getDiscription() + " Cost: " + iceCream9.getCost());
     }
 }
